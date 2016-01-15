955d3250345d001b5975e6de14b0ba82bb975241
yusuketomoto <yukmpkt@gmail.com>
2016-01-15 20:37:08 +0900

    submodule update

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ git pull origin master # check for updates to the code
 echo "ml-examples-oF: Initializing all submodules."
 git submodule init # registers submodules with git
 
+git submodule update
+
 echo "ml-examples-oF: Pulling all submodules."
 git submodule foreach git pull origin master # iterate through submodules and pull updates
 
